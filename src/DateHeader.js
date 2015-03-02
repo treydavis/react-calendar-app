56460685121e121efd0077b5a3a689fd644c7f35
Trey Davis <trey@pogoseat.com>
2015-03-02 10:44:52 -0800

    Header styling

--- src/DateHeader.js
+++ src/DateHeader.js
@@ -9,28 +9,29 @@ var DateHeader = React.createClass({
     var headerItems = [];
     
     for (var d=0; d<7; d++) {
-      var dateStyle = {
-        fontSize: 17,
-        fontWeight: "bold"
-      };
-      var day = React.createElement('span', null, days[d]);
+      var dayStyle = {};
+      var day = React.createElement('span', {style: dayStyle}, days[d]);
 
-      var dateContainerStyle = {};
+      var dateContainerStyle = {
+        paddingTop: 20
+      };
       var dateContainer = React.createElement('div', {style: dateContainerStyle}, day);
+      
       var dateHeaderItemStyle = {
         position: "absolute",
         left: (100/7.0*d).toString() + "%",
         right: (100/7.0*(7-(d+1))).toString() + "%"
-      };
-      
+      };      
       headerItems.push(React.createElement('div', {style: dateHeaderItemStyle}, dateContainer));
+      
       weekIncrement.setDate(weekIncrement.getDate() + 1);
     }
 
     var dateHeaderStyle = {
       position: "absolute",
       top: 0, right: 0, left: 50,
-      height: 50
+      height: 50,
+      borderBottom: "1px solid black"
     };
     return React.createElement('div', {style: dateHeaderStyle}, headerItems);
   }
