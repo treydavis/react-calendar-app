1149a6c738c2c9f0e14cbeade0b4fda41a316f90
Trey Davis <trey@pogoseat.com>
2015-03-01 17:40:06 -0800

    Create DateHeader to hold week day static header

---
+++ src/DateHeader.js
@@ -0,0 +1,34 @@
+var React = require('react');
+
+var DateHeader = React.createClass({
+  render: function() {
+    var dateStyle = {
+      fontSize: 17,
+      fontWeight: "bold"
+    };
+    var date = React.createElement('span', {style: dateStyle}, '22');
+    var day = React.createElement('span', null, 'Sun');
+
+    var dateContainerStyle = {};
+    var dateContainer = React.createElement('div', {style: dateContainerStyle}, [date, day]);
+
+    var headerItems = [];
+    
+    for (var d=0; d<7; d++) {
+      var dateHeaderItemStyle = {
+        position: "absolute",
+        left: (100/7.0*d).toString() + "%",
+        right: (100/7.0*(7-(d+1))).toString() + "%"
+      };
+      headerItems.push(React.createElement('div', {style: dateHeaderItemStyle}, dateContainer));      
+    }
+
+    var dateHeaderStyle = {
+      top: 0, right: 0, left: 0,
+      height: 50
+    };
+    return React.createElement('span', {style: dateHeaderStyle}, headerItems);;
+  }
+});
+
+module.exports = DateHeader;
